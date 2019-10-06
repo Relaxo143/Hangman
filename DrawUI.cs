f51b6365e7e7ca171aaedc8692bfc6c23d104b85
Relaxo143 <Relaxo143@protonmail.com>
2019-10-06 19:46:04 +0300

    Add DrawUI() skeleton
    Development of the UI and all visual aspects of the game go here - in the UI class.

---
+++ DrawUI.cs
@@ -0,0 +1,13 @@
+using Hangman;
+
+public class UI
+{
+   public static void DrawUI()
+    {
+        // code goes here
+        if (Program.isBG) // this is just an example on how to use variables in different files
+        {
+
+        }
+    }
+}
