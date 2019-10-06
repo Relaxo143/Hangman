1871c55efab63c46185adf3c872962888e830c80
Relaxo143 <Relaxo143@protonmail.com>
2019-10-06 19:42:23 +0300

    Add general program skeleton

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Hangman
+{
+    class Program
+    {
+        public static bool isBG;
+        static void Main()
+        {
+            UI.DrawUI();
+        }
+    }
+}
+
