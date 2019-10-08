c7ced099b5d32e724527c97f442a4ec2265472fa
Relaxo143 <Relaxo143@protonmail.com>
2019-10-08 23:45:45 +0300

    Write basics of the program
    Define a function for more random numbers - GetRandomNumber()
    Define a function for fetching the length of an array - GetArrayLength()
    Separate words from the word list into an array of strings
    Choose a random word with GetRandomNumber()
    Output individual characters of the chosen word to the console (debugging purposes)
    Output the letter count of the chose word to the console (debugging purposes)

--- Program.cs
+++ Program.cs
@@ -9,11 +9,87 @@ namespace Hangman
 {
     class Program
     {
-        public static bool isBG;
+        public static Random rand = new Random();
+
+        public static bool isBG = false;
+        public static char firstLetter, lastLetter;
+        public static string[] wordsEN;
+        public static string[] wordsBG;
+
+
+        static int GetArrayLenght()
+        {          
+            int counter = 0;
+
+            if (isBG)
+            {
+                foreach (string word in wordsBG)
+                {
+                    counter++;
+                }
+            }
+            else
+            {
+                foreach (string word in wordsEN)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        static int GetRandomNumber(int min, int max)
+        {
+            Thread.Sleep(rand.Next(1, 250));
+
+            int cycles = rand.Next(40000, 45889);
+            int finalNumber = 0;
+
+            for (int i = 0; i < rand.Next(1, cycles); i++)
+            {
+                if (rand.Next(1, 5000) <= 2500)
+                {
+                    finalNumber = rand.Next(min, max);
+                }
+            }
+            return finalNumber; // work to do; make the numbers more random
+        }
+
         static void Main()
         {
-            UI.DrawUI();
+            int letterCount = 0;
+            
+            if (!Program.isBG)
+            {
+                wordsEN = EN.wordList.Split(' ');
+                int randChoice = GetRandomNumber(0, GetArrayLenght());
+                string chosenWord = wordsEN[randChoice];
+
+                for (int i = 0; i < chosenWord.Length; i++)
+                {
+                    Console.WriteLine(chosenWord[i]);
+                }
+
+                letterCount = chosenWord.Length;
+                UI.DrawUI();
+            }
+            else
+            {
+                wordsBG = BG.wordList.Split(' ');
+                int randChoice = GetRandomNumber(0, GetArrayLenght());
+                string chosenWord = wordsBG[randChoice];
+
+                for (int i = 0; i < chosenWord.Length; i++)
+                {
+                    Console.WriteLine(chosenWord[i]);
+                }
+
+                letterCount = chosenWord.Length;
+                UI.DrawUI();
+            }
         }
     }
 }
+
+
 
