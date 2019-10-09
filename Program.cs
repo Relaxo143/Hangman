fd08f88ec38d51f065d5a1aa391fdaf83728b8d3
MiroBoyBG <43450825+MiroBoyBG@users.noreply.github.com>
2019-10-09 13:08:12 -0700

    Add empty gabs for the letters of the words, first and last letter on the first and last gab

--- Program.cs
+++ Program.cs
@@ -15,10 +15,13 @@ namespace Hangman
         public static char firstLetter, lastLetter;
         public static string[] wordsEN;
         public static string[] wordsBG;
+        public static int letterCount = 0;
+        public static string chosenWord;
+
 
 
         static int GetArrayLenght()
-        {          
+        {
             int counter = 0;
 
             if (isBG)
@@ -58,20 +61,66 @@ namespace Hangman
         static void Main()
         {
             int letterCount = 0;
-            
+
             if (!Program.isBG)
             {
                 wordsEN = EN.wordList.Split(' ');
                 int randChoice = GetRandomNumber(0, GetArrayLenght());
-                string chosenWord = wordsEN[randChoice];
+                 string chosenWord = wordsEN[randChoice];
 
                 for (int i = 0; i < chosenWord.Length; i++)
                 {
-                    Console.WriteLine(chosenWord[i]);
+                   ////////////////////////
                 }
 
                 letterCount = chosenWord.Length;
                 UI.DrawUI();
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("Enter a letter to guess the word!");
+                for (int i = 0; i < chosenWord.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        firstLetter = chosenWord[i];
+                    }
+                    if (i == chosenWord.Length - 1)
+                    {
+                        lastLetter = chosenWord[i];
+                    }
+                }
+                for (int l = 0; l < chosenWord.Length; l++)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    if (l == 0)
+                    {
+                        
+                        Console.WriteLine();
+                        Console.WriteLine(chosenWord[l]);
+
+                    }
+                   if(chosenWord[l] == firstLetter || chosenWord[l] == lastLetter)
+                    {
+                        if(l != 0 && l != chosenWord.Length)
+                        {
+                            Console.SetCursorPosition((l * 2) , 2);
+                            Console.WriteLine(chosenWord[l]);
+                        }
+
+                    }
+                    if (l == chosenWord.Length)
+                    {
+                       Console.SetCursorPosition((chosenWord.Length * 2) - 2, 2);
+                       Console.WriteLine();
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.SetCursorPosition((l * 2) , 3);
+                        Console.WriteLine("_");
+
+                }
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
             }
             else
             {
@@ -81,15 +130,60 @@ namespace Hangman
 
                 for (int i = 0; i < chosenWord.Length; i++)
                 {
-                    Console.WriteLine(chosenWord[i]);
+                  //////////////////////// 
                 }
 
                 letterCount = chosenWord.Length;
                 UI.DrawUI();
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("Enter a letter to guess the word!");
+                for (int i = 0; i < chosenWord.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        firstLetter = chosenWord[i];
+                    }
+                    if (i == chosenWord.Length - 1)
+                    {
+                        lastLetter = chosenWord[i];
+                    }
+                    Console.WriteLine(chosenWord[i]);
+                }
+                for (int l = 0; l < chosenWord.Length; l++)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    if (l == 0)
+                    {
+                        
+                        Console.WriteLine();
+                        Console.WriteLine(chosenWord[l]);
+
+                    }
+                    if (chosenWord[l] == firstLetter || chosenWord[l] == lastLetter)
+                    {
+                        if (l != 0 && l != chosenWord.Length)
+                        {
+                            Console.SetCursorPosition((l * 2), 2);
+                            Console.WriteLine(chosenWord[l]);
+                        }
+
+                    }
+                    if (l == chosenWord.Length)
+                    {
+                        Console.SetCursorPosition((chosenWord.Length * 2) - 2, 2);
+                        Console.WriteLine();
+                    }
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.SetCursorPosition((l * 2), 3);
+                    Console.WriteLine("_");
+
+                }
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
             }
         }
     }
 }
-
 
 
