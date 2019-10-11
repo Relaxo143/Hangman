5baa99e62f4ac329e5b393df2ee113ece5489109
Relaxo143 <Relaxo143@protonmail.com>
2019-10-11 20:31:30 +0300

    Move UI code to DrawUI()

--- DrawUI.cs
+++ DrawUI.cs
@@ -8,6 +8,51 @@ public class UI
     public static void DrawUI()
     {
 
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.WriteLine("Enter a letter to guess the word!");
+        for (int i = 0; i < Program.chosenWord.Length; i++)
+        {
+            if (i == 0)
+            {
+                Program.firstLetter = Program.chosenWord[i];
+            }
+            if (i == Program.chosenWord.Length - 1)
+            {
+                Program.lastLetter = Program.chosenWord[i];
+            }
+        }
+        for (int l = 0; l < Program.chosenWord.Length; l++)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            if (l == 0)
+            {
+
+                Console.WriteLine();
+                Console.WriteLine(" " + Program.chosenWord[l]);
+
+            }
+            if (Program.chosenWord[l] == Program.firstLetter || Program.chosenWord[l] == Program.lastLetter)
+            {
+                if (l != 0 && l != Program.chosenWord.Length)
+                {
+                    Console.SetCursorPosition((l * 2), 2);
+                    Console.WriteLine(" " + Program.chosenWord[l]);
+                }
+
+            }
+            if (l == Program.chosenWord.Length)
+            {
+                Console.SetCursorPosition((Program.chosenWord.Length * 2) - 2, 2);
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.SetCursorPosition((l * 2), 3);
+            Console.WriteLine(" _");
+
+        }
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
 
     }
 }
