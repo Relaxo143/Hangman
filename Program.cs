056d697501fb7d5407021ec2807010814a33e6ec
Relaxo143 <Relaxo143@protonmail.com>
2019-10-11 20:30:51 +0300

    Move relevant code around

--- Program.cs
+++ Program.cs
@@ -11,12 +11,12 @@ namespace Hangman
     {
         public static Random rand = new Random();
 
-        public static bool isBG = false;
+        public static bool isBG = false; //todo: let player choose this value
         public static char firstLetter, lastLetter;
         public static string[] wordsEN;
         public static string[] wordsBG;
         public static int letterCount = 0;
-        public static string chosenWord;
+        public static string chosenWord = "";
 
 
 
@@ -61,129 +61,32 @@ namespace Hangman
         static void Main()
         {
             int letterCount = 0;
+            int randChoice;
 
-            if (!Program.isBG)
+            if (Program.isBG)
             {
-                wordsEN = EN.wordList.Split(' ');
-                int randChoice = GetRandomNumber(0, GetArrayLenght());
-                 string chosenWord = wordsEN[randChoice];
-
-                for (int i = 0; i < chosenWord.Length; i++)
-                {
-                   ////////////////////////
-                }
-
-                letterCount = chosenWord.Length;
-                UI.DrawUI();
-                Console.ForegroundColor = ConsoleColor.DarkBlue;
-                Console.WriteLine("Enter a letter to guess the word!");
-                for (int i = 0; i < chosenWord.Length; i++)
-                {
-                    if (i == 0)
-                    {
-                        firstLetter = chosenWord[i];
-                    }
-                    if (i == chosenWord.Length - 1)
-                    {
-                        lastLetter = chosenWord[i];
-                    }
-                }
-                for (int l = 0; l < chosenWord.Length; l++)
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    if (l == 0)
-                    {
-                        
-                        Console.WriteLine();
-                        Console.WriteLine(chosenWord[l]);
-
-                    }
-                   if(chosenWord[l] == firstLetter || chosenWord[l] == lastLetter)
-                    {
-                        if(l != 0 && l != chosenWord.Length)
-                        {
-                            Console.SetCursorPosition((l * 2) , 2);
-                            Console.WriteLine(chosenWord[l]);
-                        }
-
-                    }
-                    if (l == chosenWord.Length)
-                    {
-                       Console.SetCursorPosition((chosenWord.Length * 2) - 2, 2);
-                       Console.WriteLine();
-                    }
-
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    Console.SetCursorPosition((l * 2) , 3);
-                        Console.WriteLine("_");
-
-                }
-                Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.DarkMagenta;
-
+                wordsBG = BG.wordList.Split(' ');
+                randChoice = GetRandomNumber(0, GetArrayLenght());
+                chosenWord = wordsBG[randChoice];             
             }
             else
             {
-                wordsBG = BG.wordList.Split(' ');
-                int randChoice = GetRandomNumber(0, GetArrayLenght());
-                string chosenWord = wordsBG[randChoice];
-
-                for (int i = 0; i < chosenWord.Length; i++)
-                {
-                  //////////////////////// 
-                }
-
-                letterCount = chosenWord.Length;
-                UI.DrawUI();
-                Console.ForegroundColor = ConsoleColor.DarkBlue;
-                Console.WriteLine("Enter a letter to guess the word!");
-                for (int i = 0; i < chosenWord.Length; i++)
-                {
-                    if (i == 0)
-                    {
-                        firstLetter = chosenWord[i];
-                    }
-                    if (i == chosenWord.Length - 1)
-                    {
-                        lastLetter = chosenWord[i];
-                    }
-                    Console.WriteLine(chosenWord[i]);
-                }
-                for (int l = 0; l < chosenWord.Length; l++)
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    if (l == 0)
-                    {
-                        
-                        Console.WriteLine();
-                        Console.WriteLine(chosenWord[l]);
-
-                    }
-                    if (chosenWord[l] == firstLetter || chosenWord[l] == lastLetter)
-                    {
-                        if (l != 0 && l != chosenWord.Length)
-                        {
-                            Console.SetCursorPosition((l * 2), 2);
-                            Console.WriteLine(chosenWord[l]);
-                        }
-
-                    }
-                    if (l == chosenWord.Length)
-                    {
-                        Console.SetCursorPosition((chosenWord.Length * 2) - 2, 2);
-                        Console.WriteLine();
-                    }
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    Console.SetCursorPosition((l * 2), 3);
-                    Console.WriteLine("_");
-
-                }
-                Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.DarkMagenta;
-
+                wordsEN = EN.wordList.Split(' ');
+                randChoice = GetRandomNumber(0, GetArrayLenght());
+                chosenWord = wordsEN[randChoice];       
             }
+
+            for (int i = 0; i < chosenWord.Length; i++)
+            {
+                ////////////////////////
+            }
+            letterCount = chosenWord.Length;
+            UI.DrawUI();
+
+
+
+
         }
     }
 }
-
 
