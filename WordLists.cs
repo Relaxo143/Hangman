3ef370e2832d8de247c4dd2c0f63ccbbb3322978
Kaloyan Tsankov <43270892+Kaloyan15@users.noreply.github.com>
2019-10-13 01:47:52 +0300

    New words, I guess

--- WordLists.cs
+++ WordLists.cs
@@ -1,9 +1,9 @@
 class EN
 {
-   public static string wordList = "hypothalamus verdict tranquilize principality country humanoid robot keyboard confident eccentric skyscraper blockchain dungeon pencil rollercoaster violin hypnotize microphone penis squirrel accountant irresponsive amphibious procreate obstruct";
+   public static string wordList = "hypothalamus tranquilize principality humanoid keyboard confident eccentric skyscraper blockchain rollercoaster hypnotize microphone squirrel accountant irresponsive amphibious procreate obstruct";
     
 }
 class BG
 {
-    public static string wordList = "ябълка динозавър сифон";
+    public static string wordList = "трансцеденталност екзистенциализъм психология интерфаза лексикон лексикология морфология биология теоцентризъм антропоцентризъм хелиоцентризъм екскремент програмиране клавиатура математика колесница магистрала валдхорна оркестър празненство подложка изменчивост унаследяване комунизъм слушалки реформация плебисцит платформа репарации референдум конституция правителство парламент ренатурация урбанизация денатурация часовник растениевъдство животновъдство металургия хексахлороциклохексан тоалетна прозорец автомонтьор тираджия йонизация компютър съдилище компактдиск перфоратор география чалготека доминион дискотека статуетка разклонител самозадоволяване кавалджия изпълнител майсторлък карагьоз синекдоха паралелизъм апосиопеза алитерация 
 }
